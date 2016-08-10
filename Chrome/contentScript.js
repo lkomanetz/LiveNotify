5d04c06f68660ec5793e22e7051c9ba2fd0f3a66
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2016-08-10 16:41:48 -0500

    Adds work to build an array of current chatting users.

--- Chrome/contentScript.js
+++ Chrome/contentScript.js
@@ -1,13 +1,21 @@
 var intervalTime = 1000; // 1 second
+var cleanupIntervalTime = 1000; // 1 second
+var maxTimeoutThresholdInHours = 1; 
 var userHandle = "Logan";
 var highlightColor = "#ff5c33";
 var shoulderTapRegex = /@([A-Za-z0-9_]+)/g;
 var highlightedMessages = [];
+var usersChatting = [];
 
+//TODO(Logan): I only want to retrive the messages that haven't been processed yet.
 var intervalHandle = setInterval(function() {
     var messages = getChatMessages();
     highlightShoulderTaps(messages);
 }, intervalTime);
+
+var cleanupIntervalHandle = setInterval(function() {
+    cleanupActiveUsers();
+}, cleanupIntervalTime);
 
 function getChatMessages() {
     return document.getElementById("chat_messages").childNodes;
@@ -32,11 +40,11 @@ function highlightShoulderTaps(messages) {
         var newHtml = messages[i].innerHTML.replace(shoulderTapRegex, createSpanElement);
         messages[i].innerHTML = newHtml;
         
-        highlightedMessages.unshift({
-            sentBy: messages[i].getElementsByClassName("user_link")[0].title,
-            sentOn: new Date(messages[i].getElementsByClassName("timeago")[0].title)
-        });
+        var msg = new Message();
+        msg.sentBy = messages[i].getElementsByClassName("user_link")[0].title;
+        msg.sentOn = new Date(messages[i].getElementsByClassName("timeago")[0].title);
         
+        highlightedMessages.unshift(msg);
         sendNotification(highlightedMessages[0]);
     }
 }
@@ -80,4 +88,57 @@ function sendNotification(message) {
         message: message.sentBy + " has tapped you."
     }, function() {});
     */
+}
+
+function buildUserChattingArray(messages) {
+    for (var i = 0; i < messages.length; i++) {
+        var user = new User();
+        user.name = messages[i].getElementsByClassName("user_link")[0].title;
+        user.lastMessageSentOn = new Date(messages[i].getElementsByClassName("timeago")[0].title);
+        
+        if (!doesUserExist(user)) {
+            usersChatting.push(user);
+        }
+    }
+}
+
+function cleanupActiveUsers() {
+    for (var i = 0; i < usersChatting.length; i++) {
+        if (!isUserActive(usersChatting[i])) {
+            usersChatting.splice(i, 1);
+        }
+    }
+}
+
+function doesUserExist(user) {
+    var foundItems = usersChatting.filter(function(item) {
+        return item.name === user.name;
+    });
+    
+    if (foundItems.length === 1) {
+        return true;
+    }
+    else {
+        return false;        
+    }
+}
+
+function isUserActive(user) {
+    var now = new Date();
+    var utcNow = new Date(now.toUTCString());
+    var differenceInMilliseconds = Math.abs(utcNow - user.lastMessageSentOn);
+    var differenceInHours = (differenceInMilliseconds / (60 * 60 * 1000));
+    
+    var isActive = (differenceInHours < maxTimeoutThresholdInHours) ? true : false;
+    return isActive;
+}
+
+function User() {
+    this.name;
+    this.lastMessageSentOn;
+}
+
+function Message() {
+    this.sentBy;
+    this.sentOn;
 }
