476931e030aadf6a86a28d21f976de768ff90e83
Logan Komanetz <Logan Komanetz>
2016-08-13 23:36:40 -0500

    Removes TODO message about removing hard-coded handle.

--- Chrome/processors/messageProcessor.js
+++ Chrome/processors/messageProcessor.js
@@ -74,7 +74,6 @@ MessageProcessor.prototype.highlightShoulderTaps = function(message) {
     var newHtml = message.innerHTML.replace(this.shoulderTapRegex, function(match) {
         var elem = match;
         
-        // TODO(Logan):  Remove hard coded handle in favor of a data driven value.
         if (match.indexOf(userHandle) !== -1) {
             elem = "<span style='color:#000000;background-color:#b80000;font-weight:800;border-radius:2px'>" + match + "</span>";
             processorContext.sendShoulderTappedEvent(message);
